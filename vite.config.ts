dc098fd5e6796922e5347ded952d91809d82e9a7
rahulyadav3766 <rahulyadav3766@gmail.com>
2024-01-21 00:42:36 +0530

    fix: config issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,59 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { PluginOption, defineConfig } from "vite";
+
+const muteWarningsPlugin = (warningsToIgnore: string[][]): PluginOption => {
+  const mutedMessages = new Set();
+  return {
+    name: "mute-warnings",
+    enforce: "pre",
+    config: (userConfig) => ({
+      build: {
+        rollupOptions: {
+          onwarn(warning, defaultHandler) {
+            if (warning.code) {
+              const muted = warningsToIgnore.find(
+                ([code, message]) =>
+                  code == warning.code && warning.message.includes(message)
+              );
+
+              if (muted) {
+                mutedMessages.add(muted.join());
+                return;
+              }
+            }
+
+            if (userConfig.build?.rollupOptions?.onwarn) {
+              userConfig.build.rollupOptions.onwarn(warning, defaultHandler);
+            } else {
+              defaultHandler(warning);
+            }
+          },
+        },
+      },
+    }),
+    closeBundle() {
+      const diff = warningsToIgnore.filter((x) => !mutedMessages.has(x.join()));
+      if (diff.length > 0) {
+        this.warn(
+          "Some of your muted warnings never appeared during the build process:"
+        );
+        diff.forEach((m) => this.warn(`- ${m.join(": ")}`));
+      }
+    },
+  };
+};
+
+const warningsToIgnore = [
+  ["SOURCEMAP_ERROR", "Can't resolve original location of error"],
+  ["INVALID_ANNOTATION", "contains an annotation that Rollup cannot interpret"],
+];
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), muteWarningsPlugin(warningsToIgnore)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
